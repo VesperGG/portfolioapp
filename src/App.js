698b44aa0ce21c35871c5569260d245bae0c738e
unknown <gerdankg@gmail.com>
2022-08-13 00:03:49 +0600

    styledcomponents commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import GLobalStyle from './cmp/GlobalStyle';
 import AboutUs from './pages/AboutUs'
+//global style
 
 function App() {
   return (
     <div className="App">
+      <GLobalStyle/>
       <AboutUs/>
     </div>
   );
