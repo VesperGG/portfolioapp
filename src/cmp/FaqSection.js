3ac566a008ceebb3700b523856f97f266c3707b8
unknown <gerdankg@gmail.com>
2022-08-12 23:36:53 +0600

    faq commit

---
+++ src/cmp/FaqSection.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>Any questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>Hwo do i start?</h4>
+                <div className="answer">
+                    <p>Answer by me my brother</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, distinctio mollitia sapiente quis qui quaerat. A corporis, officia exercitationem ab ea aut nulla in omnis corrupti. Nisi officiis asperiores officia?</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Diff payments ?</h4>
+                <div className="answer">
+                    <p>Answer by me my brother</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, distinctio mollitia sapiente quis qui quaerat. A corporis, officia exercitationem ab ea aut nulla in omnis corrupti. Nisi officiis asperiores officia?</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What product do you offer?</h4>
+                <div className="answer">
+                    <p>Answer by me my brother</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, distinctio mollitia sapiente quis qui quaerat. A corporis, officia exercitationem ab ea aut nulla in omnis corrupti. Nisi officiis asperiores officia?</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>From what do i start?</h4>
+                <div className="answer">
+                    <p>Answer by me my brother</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, distinctio mollitia sapiente quis qui quaerat. A corporis, officia exercitationem ab ea aut nulla in omnis corrupti. Nisi officiis asperiores officia?</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import AboutSection from '../cmp/AboutSection'
 import ServicesSection from '../cmp/ServicesSection';
-
+import FaqSection from '../cmp/FaqSection';
 const AboutUs = () => {
     return (
         <>
         <AboutSection/>
         <ServicesSection/>
+        <FaqSection/>
         </>
     )
 }
