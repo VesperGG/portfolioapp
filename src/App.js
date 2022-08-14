7990b058c63d4476b2dd278284785421ba8a3089
unknown <gerdankg@gmail.com>
2022-08-15 00:50:09 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import GLobalStyle from './cmp/GlobalStyle';
+import Nav from './cmp/Nav';
 import AboutUs from './pages/AboutUs'
 //global style
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <GLobalStyle/>
       <AboutUs/>
     </div>
